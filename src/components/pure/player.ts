dbb96d403b5cb25f3b300c03f667693a1450d86d
ddu <ddu6@protonmail.com>
2021-07-07 23:57:02 +0800

    update

--- src/components/pure/player.ts
+++ src/components/pure/player.ts
@@ -5,19 +5,22 @@ import * as css from '../../lib/css'
 export class Player{
     readonly element=new Div(['player'])
     readonly styleEle=document.createElement('style')
-    readonly dp:DPlayer
+    readonly dp?:DPlayer
     constructor(public parent:HTMLElement){
         this.styleEle.textContent=css.main
         parent.append(this.styleEle)
         parent.append(this.element.element)
         const params = new URLSearchParams(document.location.search)
         const src=params.get('src')??document.body.dataset.src??''
+        if(src===''){
+            return
+        }
         const type=src.endsWith('.flv')?'customFLV':'auto'
         const time=Number(params.get('t')??document.body.dataset.t??'')
         this.dp = new DPlayer({
             container: this.element.element,
             playbackSpeed:[0.5, 0.75, 1, 1.25, 1.5, 2,3,4,5],
-            video: {
+            video:{
                 url: src,
                 type:type,
                 customType: {
